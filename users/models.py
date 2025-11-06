5202c7225e36ecc6d269d7443eeed9d0b67dfe75
aditya19prakash <adyprakash19@gmail.com>
2025-11-07 01:18:47 +0530

    Add LastSeen model to track user activity with resources

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from resources.models import Resource
 class User(models.Model):
     id = models.AutoField(primary_key=True) 
     ROLE_CHOICES = (
@@ -13,3 +13,11 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
     profile_info = models.TextField(blank=True, null=True)
+
+
+class LastSeen(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    resource= models.ForeignKey(Resource,on_delete=models.CASCADE)
+    Created_at = models.DateTimeField(auto_now_add=True)
+
