fcc4bc4cc97613ab738ff7c6d6a02d89e9f6efff
aditya19prakash <adyprakash19@gmail.com>
2025-12-18 16:49:35 +0530

    update requirements

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializer
-from django.contrib.auth import authenticate,models
+from users.serializers import UserSerializer
+from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import  User
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -57,8 +57,6 @@ def logout(request):
     except Exception as e:
         return Response({"error": "Invalid refresh token"}, status=400)
     
-
-
 @api_view(["POST"])
 def refresh_token(request):
     try:
