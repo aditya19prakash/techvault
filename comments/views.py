b3e3cf80d55057cd6416336c26a694a585fd3dfb
aditya19prakash <adyprakash19@gmail.com>
2025-12-25 18:02:58 +0530

    fix: add caching for comment and nested comment views to improve performance

--- comments/views.py
+++ comments/views.py
@@ -16,6 +16,9 @@ class CommentView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self,request,id):
         cache_key = f"comment_view:{request.get_full_path()}"
+        cache_response = cache.get(cache_key)
+        if cache_response:
+            return Response(cache_response,status=200)
         resource = Resource.objects.get(id=id)
         comments = Comment.objects.filter(resource=resource, parent=None).select_related('user').prefetch_related("replies")
         result = []
@@ -32,6 +35,7 @@ class CommentView(APIView):
             return serializer
         with ThreadPoolExecutor(max_workers=20) as threads:
            result = list(threads.map(comment_list,comments))
+        cache.set(cache_key,result,timeout=60*60)
         return Response(result, status=status.HTTP_200_OK)
     
     def put(self,request):
@@ -69,6 +73,10 @@ class NestedComments(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     def get(self,request,cmt_id):
+        cache_key = f"NestedComments_view:{request.get_full_path()}"
+        cache_response = cache.get(cache_key)
+        if cache_response:
+            return Response(cache_response,status=200)
         nested_comment = Comment.objects.filter(parent_id=cmt_id)
         result = []
         def nested_comments(cme):
@@ -92,6 +100,7 @@ class NestedComments(APIView):
             return serializer
         with ThreadPoolExecutor(max_workers=20) as threads:
            result = list(threads.map(nested_comments,nested_comment))
+        cache.set(cache_key,result,timeout=60*60)
         return Response(result, status=status.HTTP_200_OK)
     
 
