cb4561eafae229fc6f9a63bd3558a948a269aa15
aditya19prakash <adyprakash19@gmail.com>
2026-01-04 20:18:35 +0530

    first commit in aditya branch

--- users/models.py
+++ users/models.py
@@ -5,5 +5,6 @@ from django.contrib.auth.models import  User
 class User_history(models.Model):
     id = models.AutoField(primary_key=True)
     resource = models.ForeignKey(Resource,null=False,blank=False,on_delete=models.CASCADE)
+    
     user = models.ForeignKey(User,null=False,blank=False,on_delete=models.CASCADE)
     timestamp = models.TimeField(auto_now_add=True)
