085e6fa9fc75061e0e2d34a3de633fd72c006539
aditya19prakash <adyprakash19@gmail.com>
2026-01-04 20:21:01 +0530

    Revert "add user history model"
    This reverts commit fea9e944aa6fec3937614b9d580752ff6416f45f.

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from resources.models import Resource
-from django.contrib.auth.models import  User
-
-class User_history(models.Model):
-    id = models.AutoField(primary_key=True)
-    resource = models.ForeignKey(Resource,null=False,blank=False,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,null=False,blank=False,on_delete=models.CASCADE)
-    timestamp = models.TimeField(auto_now_add=True)
